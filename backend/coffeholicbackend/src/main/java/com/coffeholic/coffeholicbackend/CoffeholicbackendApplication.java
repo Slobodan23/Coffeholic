1b98ad001b327733cbc71fd5353a1c6c8d149f57
Slobodan23 <slobodansivcev@gmail.com>
2026-05-09 21:29:49 +0200

    Initial commit

---
+++ backend/coffeholicbackend/src/main/java/com/coffeholic/coffeholicbackend/CoffeholicbackendApplication.java
@@ -0,0 +1,13 @@
+package com.coffeholic.coffeholicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeholicbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeholicbackendApplication.class, args);
+	}
+
+}
---
+++ backend/coffeholicbackend/src/test/java/com/coffeholic/coffeholicbackend/CoffeholicbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeholic.coffeholicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeholicbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
